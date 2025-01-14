868ea6732907be2440787306b6ee1142d733f870
Vishal-shamdasani <shamdasani46@gmail.com>
2025-01-14 23:06:38 +0530

    next progresst on the project

--- Persistent ToDo List/script.js
+++ Persistent ToDo List/script.js
@@ -1,13 +1,28 @@
-let items=["hello world","how are you"];
+let items = ["hello world", "how are you"];
 
-const itemsDiv= document.getElementById("Items")
+const itemsDiv = document.getElementById("Items")
 
-function renderItems(){
-    itemsDiv.innerHTML=null;
-    for (const [idx,item] of Object.entries(items)){
+function renderItems() {
+    itemsDiv.innerHTML = null;
+    for (const [idx, item] of Object.entries(items)) {
+        const container = document.createElement("div")
+        container.style.marginBottom = "10px"
         const text = document.createElement("p")
-        text.textContent=item;
-        itemsDiv.appendChild(text);
+        text.textContent = item;
+        text.style.display="inline"
+        text.style.paddingRight="10px"
+        const button = document.createElement("button")
+        button.textContent = "Delete"
+        button.onclick = () => removeItem(idx)
+        container.appendChild(text);
+        container.appendChild(button)
+        itemsDiv.appendChild(container)
     }
 }
+
+function loadItem() { }
+
+function saveItem() { }
+
+function removeItem(idx) { }
 renderItems()
