584d1452c9b1c1400d1c6bf14cb4ccce0e79090b
Vishal-shamdasani <shamdasani46@gmail.com>
2025-01-17 21:50:22 +0530

    made changes to show the most recently added item at the top

--- Persistent ToDo List/script.js
+++ Persistent ToDo List/script.js
@@ -1,17 +1,17 @@
 let items = [];
 
 const itemsDiv = document.getElementById("Items");
-const storageKey="Items"
+const storageKey = "Items"
 
 function renderItems() {
     itemsDiv.innerHTML = null;
-    for (const [idx, item] of Object.entries(items)) {
+    for (const [idx, item] of Object.entries(items.reverse())) {
         const container = document.createElement("div")
         container.style.marginBottom = "10px"
         const text = document.createElement("p")
         text.textContent = item;
-        text.style.display="inline"
-        text.style.paddingRight="10px"
+        text.style.display = "inline"
+        text.style.paddingRight = "10px"
         const button = document.createElement("button")
         button.textContent = "Delete"
         button.onclick = () => removeItem(idx)
@@ -21,28 +21,28 @@ function renderItems() {
     }
 }
 
-function loadItem() { 
-    const oldItems= localStorage.getItem(storageKey)
-    if(oldItems){
-        items=JSON.parse(oldItems)
+function loadItem() {
+    const oldItems = localStorage.getItem(storageKey)
+    if (oldItems) {
+        items = JSON.parse(oldItems)
     }
     renderItems()
 }
 
-function saveItem() { 
-    const itemsString=JSON.stringify(items)
-    localStorage.setItem(storageKey,itemsString)
+function saveItem() {
+    const itemsString = JSON.stringify(items)
+    localStorage.setItem(storageKey, itemsString)
 }
 
 function addItem() {
-    temp=document.getElementById("itemInput")
-    if (temp.value===""){
+    temp = document.getElementById("itemInput")
+    if (temp.value === "") {
         alert("you can't add a empty item to the list")
-        return 
+        return
     }
-    else{
+    else {
         items.push(temp.value)
-        temp.value=""
+        temp.value = ""
     }
     saveItem()
     renderItems()
@@ -50,8 +50,8 @@ function addItem() {
 
 
 function removeItem(idx) {
-    items.splice(idx,1)
+    items.splice(idx, 1)
     saveItem()
     renderItems()
- }
-document.addEventListener("DOMContentLoaded",loadItem)
+}
+document.addEventListener("DOMContentLoaded", loadItem)
